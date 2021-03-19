ee30f5ca213f4c9708a6c87bd517d8a69000b71d
ana beatriz da silva <aanabeeatrizs@gmail.com>
2021-03-19 08:20:04 -0300

    first commit

---
+++ src/test/java/com/gft/fs/FsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
